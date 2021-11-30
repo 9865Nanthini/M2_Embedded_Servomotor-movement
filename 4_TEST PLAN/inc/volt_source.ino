a4003f4548aedf400feb2281305fc36712377c27
9865Nanthini <94334604+9865Nanthini@users.noreply.github.com>
2021-11-30 11:24:43 +0530

    Add files via upload

---
+++ 4_TEST PLAN/inc/volt_source.ino
@@ -0,0 +1,23 @@
+#define  F_CPU 1000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    DDRB |= 0xFF;
+
+    ICR1 = 19999;
+    TCCR1A |= 1<<WGM11 | 1<<COM1A1 | 1<<COM1A0 | 1<<COM1B1 | 1<<COM1B0;
+    TCCR1B |= 1<<WGM13 | 1<<WGM12 | 1<<CS10; //Prescaler=1,inverted, fast PWM,Mode 14
+    
+    while(1)
+    {
+        for( int i=1000; i<=2000; i+=100 )
+        {
+            OCR1A = ICR1 - i;
+            OCR1B = ICR1 - i;
+            _delay_ms(100);
+        }
+    }
+}
